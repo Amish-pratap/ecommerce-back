932c1a9eaaf5ffd678518d891e661e01b5bd5df9
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-08-03 15:05:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+//import routes
+const userRoutes = require('./routes/user')
+
+
+
+
+//app
+const app = express()
+
+
+//db
+mongoose
+    .connect(process.env.DATABASE, {})
+    .then(() => console.log('DB connected'))
+    .catch((err) => console.log('DB Error =>', err));
+
+//routes middleware
+app.use('/api', userRoutes);
+
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
